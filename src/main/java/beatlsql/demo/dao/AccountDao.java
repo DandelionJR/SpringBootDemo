be17dbf95a3bdb8703d6089e9f6d37152e7d7ab6
DandelionJR <luyongjie1115@163.com>
2018-03-23 13:18:20 +0800

    Spring整合beatlsql

---
+++ src/main/java/beatlsql/demo/dao/AccountDao.java
@@ -0,0 +1,10 @@
+package beatlsql.demo.dao;
+
+import beatlsql.demo.Account;
+import org.beetl.sql.core.annotatoin.SqlStatement;
+import org.beetl.sql.core.mapper.BaseMapper;
+
+public interface AccountDao extends BaseMapper<Account> {
+    @SqlStatement(params = "name")
+    Account selectAccountByName(String name);
+}
